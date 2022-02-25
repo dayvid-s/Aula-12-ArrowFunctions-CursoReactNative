98fad228cb660a3dc0a9e9ff0fead8f117916742
Dayvid Moreno Dos Santos <dayvid7santos2002@outlook.com>
2022-02-24 23:07:09 -0300

    first commit

--- App.js
+++ App.js
@@ -2,29 +2,50 @@
 import React, {Component} from 'react';
 import {Text, View, Image, StyleSheet} from 'react-native';
 import estilos from './styles/styles.js';
-import Caixas from './components/caixas.js';
 
+/*
+a próxima função em arrow function fica assim:
+(n)=>{return n*2}
+ou, caso tenha apenas um parametro
+n =>{return n*2}
+
+(n1,n2)=>{return n1 + n2}
+*/
+
+// function dobro(n) {
+//   return n * 2;
+//   ao inves disso
+//    faça arrow function
+//  desse jeito
+const dobro = n => {
+  return n * 2;
+};
+//ou de forma mais reduzida ainda
+// const dobro =n=>n*2   isso porque só tem um parametro
+
+// aqui está uma função antiga
+// function soma(n1, n2) {
+//   return n1 + n2;
+// }
+// versão dela nova
+const soma = (n1, n2) => {
+  return n1 + n2;
+};
+//ou de forma reduzida
+// const soma= (n1,n2)=> n1+n2/
 export default class App1 extends Component {
   render() {
     let vexibir = true;
     return (
       <View style={estilos.container}>
-        {/* <Image
-          source={{
-            uri: 'https://meups.com.br/noticias/imagens-fifa-22-aparecem-internet-detalhes/',
-          }}
-          style={styles.logo}
-        /> */}
-        {/* OBS: TENTEI DESSA FORMA E DEU ERRADO. */}
-        {/* Você pode pegar um arquivo do pc ou da net, com uri */}
+        <Text>{dobro(2)}</Text>
+        <Text>{soma(2, 4)}</Text>
         <Image
           style={{width: 200, height: 200}}
           source={{
             uri: 'https://meups.com.br/wp-content/uploads/2021/06/FIFA-22.jpg.webp',
           }}
         />
-        {/* <Image source={require('./assets/itsHere.png')} style={styles.logo} /> */}
-        <Caixas exibir={vexibir} />
         <Text style={estilos.textoPadrao}>youtube.com/cfbcursos </Text>
         <Text style={estilos.textoTitulo}>cfbcursos.com.br</Text>
         {vexibir ? <Text>Curso de react native</Text> : <Text>- - -</Text>}
